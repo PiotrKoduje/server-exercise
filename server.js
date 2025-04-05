3c2c73578abc731b0db7c08508c3e555a9cbd93b
Piotr Pelc <piotrpelc83@gmail.com>
2025-04-05 17:25:00 +0200

    Add middleware for link /user

--- server.js
+++ server.js
@@ -3,7 +3,30 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname), '/public'));
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use('/user', (req, res, next) => {
+  return res.show('forbidden.html');
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+  res.show('index.html');
+});
+
+app.get('/home', (req, res) => {
+  res.show('index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
 
 app.listen(7000, () => {
   console.log('Server is running on port: 7000');
